4ecdd7aec12cdcdf728b9bffd281469228c87991
scriptSQD <73490413+scriptSQD@users.noreply.github.com>
2022-03-03 23:07:09 +0300

    [I] - Fix styling for button component.

--- projects/squi/src/lib/button/button.component.ts
+++ projects/squi/src/lib/button/button.component.ts
@@ -3,7 +3,7 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'squi-button',
   templateUrl: './button.component.html',
-  styleUrls: ['./button.component.scss'],
+  styleUrls: ['./button.component.scss', '../../../styles/styles.scss'],
 })
 export class ButtonComponent {
   @Input('squi-href') href!: string;
